72e55b06c6c8b6c8acababd0da9932e0414fc1fa
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-07-03 17:51:13 +0300

    Correction on emp_homepage

--- emp_homepage.php
+++ emp_homepage.php
@@ -2,17 +2,18 @@
 include 'config.php';
 
 session_start();
-    if(isset($_SESSION['id'])) {
-        $this_user = $_SESSION["id"];
-        $us = mysqli_query($conn, "select * from user where id_user='$this_user' and type='admin'");
-        while ($row = mysqli_fetch_array($us, MYSQLI_ASSOC)) {
-            $location = "/VacationSystem/homepage.php";
-            header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
-        }
-    }else {
-        $location = "/VacationSystem/login.php";
+if(isset($_SESSION['id'])) {
+    $this_user = $_SESSION["id"];
+    $us = mysqli_query($conn, "select * from user where id_user='$this_user' and type='admin'");
+    while ($row = mysqli_fetch_array($us, MYSQLI_ASSOC)) {
+        $location = "/VacationSystem/homepage.php";
         header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
     }
+}
+else {
+    $location = "/VacationSystem/login.php";
+    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -44,28 +45,30 @@ session_start();
             <th>Status</th>
         </tr>
         <?php
-        $this_user=$_SESSION["id"];
-        $us = mysqli_query($conn,"select * from does where id_user='$this_user'");
-        while ($row = mysqli_fetch_array($us, MYSQLI_ASSOC)) {
-            $id_ap=$row["id_application"];
-            $u = mysqli_query($conn,"select * from application where id_application='$id_ap' order by date_sub desc");
-            while ($row = mysqli_fetch_array($u, MYSQLI_ASSOC)) {
-                $date_sub=$row["date_sub"];
-                $ds=date("d/m/Y",strtotime($date_sub));
-                $start = $row["vacation_start"];
-                $s=date("d/m/Y",strtotime($start));
-                $end =$row["vacation_end"];
-                $en=date("d/m/Y",strtotime($end));
-                $diff = abs(strtotime($end) - strtotime($start))/86400+1 ;
-                $r=$row["reason"];
-                $st=$row["status"];
-                echo "<tr>
+        if(isset($_SESSION['id'])) {
+            $this_user = $_SESSION["id"];
+            $us = mysqli_query($conn, "select * from does where id_user='$this_user'");
+            while ($row = mysqli_fetch_array($us, MYSQLI_ASSOC)) {
+                $id_ap = $row["id_application"];
+                $u = mysqli_query($conn, "select * from application where id_application='$id_ap' order by date_sub desc");
+                while ($row = mysqli_fetch_array($u, MYSQLI_ASSOC)) {
+                    $date_sub = $row["date_sub"];
+                    $ds = date("d/m/Y", strtotime($date_sub));
+                    $start = $row["vacation_start"];
+                    $s = date("d/m/Y", strtotime($start));
+                    $end = $row["vacation_end"];
+                    $en = date("d/m/Y", strtotime($end));
+                    $diff = abs(strtotime($end) - strtotime($start)) / 86400 + 1;
+                    $r = $row["reason"];
+                    $st = $row["status"];
+                    echo "<tr>
                 <td>$ds</td>
                 <td>$s- $en</td> 
                 <td>$r</td>
                 <td>$diff</td>    
                 <td>$st</td>
               </tr>";
+                }
             }
         }
         ?>
